69f6e6d9d6e0fabf30d8d912ca910cf05ff8fff0
Julia Luo <julialuo@andrew.cmu.edu>
2018-02-20 23:56:04 -0500

    scopes and required fields

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ActiveRecord::Base
+	belongs_to :category
+
+	validates :title, presence: true
+  	validates :content, presence: true
+
+  	scope :alphabetical, order('title')
+  	scope :active, where('active = ?', true)
+end
